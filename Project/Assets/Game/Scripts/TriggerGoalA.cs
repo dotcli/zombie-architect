687a5cae9fd697ab368e42f5fa3cfc7d0e874a69
Changbai Li <lcb931023@gmail.com>
2020-08-07 04:49:23 +0800

    fix environment issue with goalB collider; adjust rewarding method; add new models

--- Project/Assets/Game/Scripts/TriggerGoalA.cs
+++ Project/Assets/Game/Scripts/TriggerGoalA.cs
@@ -4,17 +4,46 @@ using UnityEngine;
 
 public class TriggerGoalA : MonoBehaviour {
 	LevelManager _Level;
+	public List<ZomboxAgent> teamA = new List<ZomboxAgent>();
+	public List<ZomboxAgent> teamB = new List<ZomboxAgent>();
+	
 	void Awake() {
 		_Level = FindObjectOfType<LevelManager>();
+		GameObject[] arrAgentObjA = GameObject.FindGameObjectsWithTag("agentA");
+		foreach (GameObject agentObj in arrAgentObjA)
+		{
+			ZomboxAgent agentScript = agentObj.GetComponent<ZomboxAgent>();
+			teamA.Add(agentScript);
+		}
+
+		GameObject[] arrAgentObjB = GameObject.FindGameObjectsWithTag("agentB");
+		foreach (GameObject agentObj in arrAgentObjB)
+		{
+			ZomboxAgent agentScript = agentObj.GetComponent<ZomboxAgent>();
+			teamB.Add(agentScript);
+		}
 	}
 	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.tag == "block") {
 			_Level.registerGoalA();
-            // find which agent is responsible for the goal,
-            // and reward them.
-            ZomboxBall ball = col.gameObject.GetComponent<ZomboxBall>();
-            ball.RewardAgent(ZomboxTeam.A);
-            ball.Reset();
-        }
+			// reward the team
+			foreach (ZomboxAgent agent in teamA)
+			{
+				agent.GetRewardedForGoal(ZomboxTeam.A);
+			}
+			// punish the other team
+			foreach (ZomboxAgent agent in teamB)
+			{
+				agent.GetRewardedForGoal(ZomboxTeam.A);
+			}
+			
+			
+			ZomboxBall ball = col.gameObject.GetComponent<ZomboxBall>();
+			// find which agent is responsible for the goal,
+			// and reward them.
+			// ball.RewardAgent(ZomboxTeam.A);
+			ball.Reset();
+			
+		}
 	}
 }
--- Project/Assets/Game/Scripts/TriggerGoalB.cs
+++ Project/Assets/Game/Scripts/TriggerGoalB.cs
@@ -4,17 +4,44 @@ using UnityEngine;
 
 public class TriggerGoalB : MonoBehaviour {
 	LevelManager _Level;
+	public List<ZomboxAgent> teamA = new List<ZomboxAgent>();
+	public List<ZomboxAgent> teamB = new List<ZomboxAgent>();
+	
 	void Awake() {
 		_Level = FindObjectOfType<LevelManager>();
+		GameObject[] arrAgentObjA = GameObject.FindGameObjectsWithTag("agentA");
+		foreach (GameObject agentObj in arrAgentObjA)
+		{
+			ZomboxAgent agentScript = agentObj.GetComponent<ZomboxAgent>();
+			teamA.Add(agentScript);
+		}
+
+		GameObject[] arrAgentObjB = GameObject.FindGameObjectsWithTag("agentB");
+		foreach (GameObject agentObj in arrAgentObjB)
+		{
+			ZomboxAgent agentScript = agentObj.GetComponent<ZomboxAgent>();
+			teamB.Add(agentScript);
+		}
 	}
 	void OnTriggerEnter(Collider col) {
 		if (col.gameObject.tag == "block") {
 			_Level.registerGoalB();
-            // find which agent is responsible for the goal,
-            // and reward them.
-            ZomboxBall ball = col.gameObject.GetComponent<ZomboxBall>();
-            ball.RewardAgent(ZomboxTeam.B);
-            ball.Reset();
-        }
+			// reward the team
+			foreach (ZomboxAgent agent in teamA)
+			{
+				agent.GetRewardedForGoal(ZomboxTeam.B);
+			}
+			// punish the other team
+			foreach (ZomboxAgent agent in teamB)
+			{
+				agent.GetRewardedForGoal(ZomboxTeam.B);
+			}
+
+			ZomboxBall ball = col.gameObject.GetComponent<ZomboxBall>();
+			// find which agent is responsible for the goal,
+			// and reward them.
+			// ball.RewardAgent(ZomboxTeam.B);
+			ball.Reset();
+		}
 	}
 }
